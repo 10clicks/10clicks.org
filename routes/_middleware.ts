72b9e3ec96281c4de88513658c8bc79a56f848c8
bdeweesevans <bdeweesevans@gmail.com>
2023-02-06 21:59:50 -0800

    YAY! remember to migrate to GA4 on July 1, 2023

--- routes/_middleware.ts
+++ routes/_middleware.ts
@@ -2,7 +2,7 @@ import { MiddlewareHandlerContext } from "$fresh/server.ts";
 import { createReporter } from "https://deno.land/x/g_a/mod.ts";
 import { ConnInfo } from "https://deno.land/std/http/server.ts";
 
-const ga = createReporter({ id: "G-ZJ2X1Z471N" });
+const ga = createReporter({ id: "UA-256171184-1" });
 
 interface State {
   data: string;
