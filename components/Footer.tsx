88750c8c91ce29edcae2c1d5ef8b142d2a0f5ff0
Oleksandr Gorpynich <oleksandr.gorpynich@gmail.com>
2023-03-01 11:24:03 -0800

    Modified footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,15 @@
 export default function Footer() {
-    return (
-        <><div id="copyright" align="center">&copy; 2022 - 2023 www.10clicks.org</div>
-        <script>
+  /*
+  <script>
         (function(){
             let cpr = document.getElementById("copyright");
             cpr.innerHTML = "&copy; 2022 - "+new Date().getFullYear()+" www.10clicks.org - All Rights Reserved.";
         })();
-        </script></>
-    )
+        </script>
+  */
+  return (
+    <>
+      <div id="copyright">&copy; 2022 - 2023 www.10clicks.org</div>
+    </>
+  )
 }
